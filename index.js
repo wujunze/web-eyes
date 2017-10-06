b393c56dd528b02a1948cb312545b5b6f90ba5d5
wujunze <itwujunze@163.com>
2017-10-06 11:27:12 +0800

    fix codestyle

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var express = require("express");
 var app = express();
 var hostName = '127.0.0.1';
 var port = 8080;
-
 const wappalyzer = require('wappalyzer');
-
 app.get('/', function (req, res) {
     var url = req.query.url;
     console.warn(url);
@@ -15,7 +13,6 @@ app.get('/', function (req, res) {
             res.end(JSON.stringify(json));
         });
 });
-
 app.listen(port, hostName, function () {
     console.log("应用实例，访问地址为 http://%s:%s", hostName, port)
 });
