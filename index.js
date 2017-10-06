c52c6a4225bae0711c4b2e31891d6aad676e5948
wujunze <itwujunze@163.com>
2017-10-06 10:07:55 +0800

    init project

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const wappalyzer = require('wappalyzer');
+
+const args = process.argv.slice(2);
+
+const url = args[0] || '';
+
+if ( !url ) {
+    process.stderr.write('No URL specified\n');
+
+    process.exit(1);
+}
+
+wappalyzer.analyze(url)
+    .then(json => {
+    process.stdout.write(JSON.stringify(json) + '\n')
+
+process.exit();
+})
+.catch(error => {
+    throw error
+});
